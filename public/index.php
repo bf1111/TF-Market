15425703d62f323e90ceff8a307c77497d1e0e66
xiaoming <xiaoming@qq.com>
2020-07-06 17:47:31 +0800

    中间件

--- public/index.php
+++ public/index.php
@@ -13,9 +13,10 @@
 namespace think;
 
 //解决跨域问题
-header('Content-Type: text/html;charset=utf-8');
-header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
-header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE');
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
+
 
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
